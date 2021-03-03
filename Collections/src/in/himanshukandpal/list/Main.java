806bd0e5bf360ec8c3d892d5d74dbba07bf1f38b
Himanshu Kandpal <cse10324.sbit@gmail.com>
2021-03-03 22:48:13 +0530

    arra

---
+++ Collections/src/in/himanshukandpal/list/Main.java
@@ -0,0 +1,13 @@
+package in.himanshukandpal.list;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(12);
+        arrayList.add(14);
+        arrayList.add(15);
+    }
+}
