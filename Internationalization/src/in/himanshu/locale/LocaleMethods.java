38f84650c243492cabd2f73ba821be53026625e3
Himanshu Kandpal <cse10324.sbit@gmail.com>
2021-04-03 22:59:26 +0530

    internationalization module setup

---
+++ Internationalization/src/in/himanshu/locale/LocaleMethods.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021.
+ * Developer: Himanshu Kandpal
+ * Date: 03/04/21, 10:58 PM
+ * Email: hk@himanshukandpal.in
+ * Github: https://github.com/himanshuKp
+ */
+
+package in.himanshu.locale;
+
+public class LocaleMethods {
+}
