7b98f8d6627ea146d7647c99440c4122758d676a
Himanshu Kandpal <cse10324.sbit@gmail.com>
2021-04-20 15:03:59 +0530

    added enqueue method and default size implementation

--- DSA/src/in/himanshukandpal/queues/Queue.java
+++ DSA/src/in/himanshukandpal/queues/Queue.java
@@ -9,16 +9,33 @@
 package in.himanshukandpal.queues;
 
 public class Queue {
+    private static final int DEFAULT_MAX_SIZE = Integer.MAX_VALUE;
+    private final int maxSize;
     public LinkedList queue;
     public int size;
 
     public Queue() {
+        this(DEFAULT_MAX_SIZE);
+    }
+
+    public Queue(int defaultMaxSize) {
         this.queue = new LinkedList();
         this.size = 0;
+        this.maxSize = defaultMaxSize;
+    }
+
+    public void enqueue(String data){
+        this.queue.addToTail(data);
+        this.size += 1;
+
+        System.out.println("Added "+data+"! Queue size is now "+this.size);
     }
 
     public static void main(String[] args) {
-        Queue queue = new Queue();
-        System.out.println("This queue has "+queue.size+" nodes.");
+        Queue coffeeOrder = new Queue();
+        System.out.println("coffeeOrder queue has " + coffeeOrder.size + " orders.");
+        coffeeOrder.enqueue("latte");
+        coffeeOrder.enqueue("espresso");
+        coffeeOrder.enqueue("cappuccino");
     }
 }
