892d1f8dac48454bbfec1bd6791fa482c08c7ffb
Himanshu Kandpal <cse10324.sbit@gmail.com>
2021-05-02 22:34:51 +0530

    hashCode working

--- DSA/src/in/himanshukandpal/hashmap/HashMap.java
+++ DSA/src/in/himanshukandpal/hashmap/HashMap.java
@@ -17,8 +17,7 @@ public class HashMap {
 
     public static void main(String[] args) {
         HashMap newHashMap = new HashMap(3);
-        System.out.println(newHashMap.hash("c"));
-        System.out.println(newHashMap.hash("ca"));
+        System.out.println(newHashMap.hash("car"));
         System.out.println(newHashMap.hash("car"));
     }
 
@@ -27,6 +26,7 @@ public class HashMap {
         for (int i = 0; i < key.length(); i++) {
             hashCode += Character.codePointAt(key, i);
         }
+        hashCode = hashCode % this.hashMap.length;
         return hashCode;
     }
 }
