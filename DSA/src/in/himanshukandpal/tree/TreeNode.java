f5ecac3b7d65ac484c8edc482c8659d22e8736cc
Himanshu Kandpal <cse10324.sbit@gmail.com>
2021-06-02 14:15:58 +0530

    added remove child 2

--- DSA/src/in/himanshukandpal/tree/TreeNode.java
+++ DSA/src/in/himanshukandpal/tree/TreeNode.java
@@ -27,4 +27,31 @@ public class TreeNode {
         TreeNode child = new TreeNode(childData);
         this.children.add(child);
     }
+
+    public void removeChild(TreeNode childToRemove){
+         if(this.children.isEmpty()){
+             return;
+         } else if (this.children.contains(childToRemove)){
+             this.children.remove(childToRemove);
+             return;
+         } else {
+             for (TreeNode child: this.children){
+                 child.removeChild(childToRemove);
+             }
+         }
+    }
+
+    public void removeChild(Object data){
+        if (this.children.isEmpty()){
+            return;
+        }
+        for (TreeNode child: this.children){
+            if(child.data == data){
+                removeChild(data);
+            }
+        }
+        for (TreeNode child: this.children){
+            child.removeChild(data);
+        }
+    }
 }
