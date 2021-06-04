74299035cba1a4610f643359b7231ecd3506abfa
Himanshu Kandpal <cse10324.sbit@gmail.com>
2021-06-04 20:18:21 +0530

    days year calculator

---
+++ basics/src/in/himanshukandpal/challenges/MinutesCalculator.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2021.
+ * Developer: Himanshu Kandpal
+ * Date: 04/06/21, 8:04 PM
+ * Email: hk@himanshukandpal.in
+ * Github: https://github.com/himanshuKp
+ */
+/*
+Write a method printYearsAndDays with parameter of type long named minutes.
+
+The method should not return anything (void) and it needs to calculate the years and days from the minutes parameter.
+
+If the parameter is less than 0, print text "Invalid Value".
+
+Otherwise, if the parameter is valid then it needs to print a message in the format "XX min = YY y and ZZ d".
+
+XX represents the original value minutes.
+YY represents the calculated years.
+ZZ represents the calculated days.
+
+
+EXAMPLES OF INPUT/OUTPUT:
+
+* printYearsAndDays(525600);  → should print "525600 min = 1 y and 0 d"
+
+* printYearsAndDays(1051200); → should print "1051200 min = 2 y and 0 d"
+
+* printYearsAndDays(561600);  → should print "561600 min = 1 y and 25 d"
+
+
+TIPS:
+
+* Be extra careful about spaces in the printed message.
+
+* Use the remainder operator
+
+* 1 hour = 60 minutes
+
+* 1 day = 24 hours
+
+* 1 year = 365 days
+* */
+package in.himanshukandpal.challenges;
+
+public class MinutesCalculator {
+
+    public static void printYearsAndDays(long minutes){
+        if (minutes < 0){
+            System.out.println("Invalid Value");
+        } else {
+
+            long hour = (minutes / 60);
+            long days = hour / 24;
+            long years = days / 365;
+            long remainingDays = days % 365;
+
+            System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+        }
+    }
+
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+    }
+}
