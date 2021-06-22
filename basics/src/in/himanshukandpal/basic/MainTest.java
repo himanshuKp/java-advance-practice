4f42502f6f0ca673a627fa014188263254140636
Himanshu Kandpal <cse10324.sbit@gmail.com>
2021-06-22 18:01:01 +0530

    static test

---
+++ basics/src/in/himanshukandpal/basic/MainTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2021.
+ * Developer: Himanshu Kandpal
+ * Date: 22/06/21, 5:59 PM
+ * Email: hk@himanshukandpal.in
+ * Github: https://github.com/himanshuKp
+ */
+
+package in.himanshukandpal.basic;
+
+public class MainTest {
+
+    static void print(){
+        System.out.println("Test print method.");
+    }
+
+    public static void main(String[] args) {
+        print();
+    }
+}
