8bb6bfdd186342419d1f2fe952d39f43ec4af55c
Himanshu Kandpal <cse10324.sbit@gmail.com>
2021-07-08 18:43:53 +0530

    static block test

---
+++ basics/src/in/himanshukandpal/basic/StaticTest.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021.
+ * Developer: Himanshu Kandpal
+ * Date: 08/07/21, 6:42 PM
+ * Email: hk@himanshukandpal.in
+ * Github: https://github.com/himanshuKp
+ */
+
+package in.himanshukandpal.basic;
+
+public class StaticTest {
+    static {
+        System.out.println("static block");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("main block");
+    }
+}
