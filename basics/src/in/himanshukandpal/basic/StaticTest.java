5e0d1a3bd3c0b9e413159ed827b5b6379ba892d3
Himanshu Kandpal <cse10324.sbit@gmail.com>
2021-07-08 18:47:18 +0530

    static block test

--- basics/src/in/himanshukandpal/basic/StaticTest.java
+++ basics/src/in/himanshukandpal/basic/StaticTest.java
@@ -13,7 +13,12 @@ public class StaticTest {
         System.out.println("static block");
     }
 
+    public static void test(){
+        System.out.println("test static method.");
+    }
+
     public static void main(String[] args) {
         System.out.println("main block");
+        test();
     }
 }
