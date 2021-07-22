56c0f044fb8ca02ff5cbbf01e8a73d7c0016325f
Himanshu Kandpal <cse10324.sbit@gmail.com>
2021-07-22 16:03:11 +0530

    runnable using lambda

---
+++ basics/src/in/himanshukandpal/lambda/RunnableLambdaExample.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021.
+ * Developer: Himanshu Kandpal
+ * Date: 22/07/21, 4:01 PM
+ * Email: hk@himanshukandpal.in
+ * Github: https://github.com/himanshuKp
+ */
+
+package in.himanshukandpal.lambda;
+
+public class RunnableLambdaExample {
+    public static void main(String[] args) {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Runnable anonymous method.");
+            }
+        });
+        t.run();
+
+        Thread t1 = new Thread(()-> System.out.println("Runnable lambda method."));
+        t1.run();
+    }
+}
